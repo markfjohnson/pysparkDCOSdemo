69aaf44737daff27dad4a89d997d613b449f8b13
Mark Johnson <mjohnson@mesosphere.com>
2017-06-28 10:46:05 -0400

    adding some test programs

---
+++ test.py
@@ -0,0 +1,10 @@
+from __future__ import print_function
+
+import sys
+from random import random
+from operator import add
+
+from pyspark.sql import SparkSession
+
+
+if __name__ == "__main__":
---
+++ test.py.sh
@@ -0,0 +1,2 @@
+dcos spark run --submit-args="https://raw.githubusercontent.com/markfjohnson/pysparkDCOSdemo/master/test.py" --verbose
+
