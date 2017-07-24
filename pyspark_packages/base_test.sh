03e7318b41bfd240ae19492f095aec3dbc99ce98
markjohnson <markfjohnson@gmail.com>
2017-07-24 16:03:49 -0400

    update

---
+++ pyspark_packages/base_test.sh
@@ -0,0 +1,2 @@
+dcos spark run --submit-args="https://raw.githubusercontent.com/markfjohnson/pysparkDCOSdemo/master/pandas_pyspark.py" --verbose
+
---
+++ pyspark_packages/pandas_pyspark.sh
@@ -0,0 +1,2 @@
+dcos spark run --docker-image="markfjohnson/spark_pandas" --submit-args="https://raw.githubusercontent.com/markfjohnson/pysparkDCOSdemo/master/pandas_pyspark.py" --verbose 
+
